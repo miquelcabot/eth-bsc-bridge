2e88e0abace3af97212909b9f7246263f3a7b349
Miquel Àngel Cabot <miquel@unizen.io>
2021-11-11 18:42:23 +0100

    chore: execute and read event on bridge

--- scripts/bridge-eth-bsc.ts
+++ scripts/bridge-eth-bsc.ts
@@ -9,13 +9,25 @@ import * as BRIDGEETH from '../deployments/rinkeby/BridgeETH.json';
  */
 async function main() {
   const [owner] = await ethers.getSigners();
+  const amount = 1000;
 
   const BridgeETH = await ethers.getContractFactory('BridgeETH');
   const bridgeETH = BridgeETH.attach(BRIDGEETH.address);
 
-  bridgeETH.on('Transfer', () => {  // When we detect the Transfer event
+  bridgeETH.on('Transfer', (from, to, amount, date, nonce, step) => {  // When we detect the Transfer event
     console.log('Transfer');
+    console.log(`from: ${from}`);
+    console.log(`to: ${to}`);
+    console.log(`amount: ${amount}`);
+    console.log(`date: ${date}`);
+    console.log(`nonce: ${nonce}`);
+    console.log(`step: ${step}`);
   });
+
+  const tx = await bridgeETH.burn(owner.address, amount);
+  const receipt = await tx.wait();
+  console.log('transactionHash: ', receipt.transactionHash);
+  console.log('logs: ', JSON.stringify(receipt.logs));
 }
 
 main()
