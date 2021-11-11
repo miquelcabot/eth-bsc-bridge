28636b17a3274b6da856bea572133a3defafa28c
Miquel Àngel Cabot <miquel@unizen.io>
2021-11-11 15:25:00 +0100

    chore: add balance of tokenETH script

---
+++ scripts/balance-token-eth.ts
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import '@nomiclabs/hardhat-ethers';
+import { ethers } from 'hardhat';
+import { TokenETH } from '../typechain/TokenETH';
+// eslint-disable-next-line camelcase
+import { TokenETH__factory } from '../typechain/factories/TokenETH__factory';
+// Addresses in Mainnet
+import * as TOKENETH from '../deployments/rinkeby/TokenETH.json';
+
+/**
+ * main function
+ */
+async function main() {
+  const [owner] = await ethers.getSigners();
+
+  const tokenETH: TokenETH = TokenETH__factory.connect(TOKENETH.address, owner);
+
+  const balance = await tokenETH.balanceOf(owner.address);
+  console.log(`Balance of ${owner.address}: ${balance}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
