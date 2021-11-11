add593f52badb75c272be4d6adbadd6075de4aa0
Miquel Àngel Cabot <miquel@unizen.io>
2021-11-11 16:06:19 +0100

    chore: mint and setowner when deploying

--- deploy/00_deploy.ts
+++ deploy/00_deploy.ts
@@ -3,6 +3,12 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { ethers } from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { TokenETH } from '../typechain/TokenETH';
+// eslint-disable-next-line camelcase
+import { TokenETH__factory } from '../typechain/factories/TokenETH__factory';
+import { TokenBSC } from '../typechain/TokenBSC';
+// eslint-disable-next-line camelcase
+import { TokenBSC__factory } from '../typechain/factories/TokenBSC__factory';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts, getChainId } = hre;
@@ -26,12 +32,24 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       log: true
     });
 
+    // Mint to owner
+    const tokenETHContract: TokenETH = TokenETH__factory.connect(tokenETH.address, owner);
+    await tokenETHContract.mint(
+      owner.address,
+      ethers.utils.parseEther('1000')
+    );
+
     // Deploy BridgeETH
-    await deploy('BridgeETH', {
+    const bridgeETH = await deploy('BridgeETH', {
       from: deployer,
       args: [tokenETH.address],
       log: true
     });
+
+    // Set owner to bridgeETH
+    await tokenETHContract.updateOwner(
+      bridgeETH.address
+    );
   } else if (chainId == '97') { // ----- bsctest -------------------------------
     console.log(`Deploying to chainId ${chainId} - BSCtest`);
 
@@ -42,13 +60,19 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       log: true
     });
 
-    await deploy('BridgeBSC', {
+    // Deploy BridgeBSC
+    const bridgeBSC = await deploy('BridgeBSC', {
       from: deployer,
       args: [tokenBSC.address],
       log: true
     });
-  }
 
+    // Set owner to bridgeBSC
+    const tokenBSCContract: TokenBSC = TokenBSC__factory.connect(tokenBSC.address, owner);
+    await tokenBSCContract.updateOwner(
+      bridgeBSC.address
+    );
+  }
 };
 
 func.tags = ['TokenETH', 'BridgeETH', 'TokenBSC', 'BridgeBSC'];
