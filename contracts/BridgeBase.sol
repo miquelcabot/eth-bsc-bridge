31a23ef96dbb39b479122be5c773b48b220f6205
Miquel Àngel Cabot <miquel@unizen.io>
2021-11-16 12:16:58 +0100

    chore: add signature to burn and mint functions

--- contracts/BridgeBase.sol
+++ contracts/BridgeBase.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
 import "./IToken.sol";
@@ -5,8 +6,7 @@ import "./IToken.sol";
 contract BridgeBase {
     address public owner;
     IToken public token;
-    uint256 public nonce;
-    mapping(uint256 => bool) public processedNonces;
+    mapping(address => mapping(uint256 => bool)) public processedNonces;
 
     enum Step {
         Burn,
@@ -19,6 +19,7 @@ contract BridgeBase {
         uint256 amount,
         uint256 date,
         uint256 nonce,
+        bytes signature,
         Step indexed step
     );
 
@@ -27,42 +28,102 @@ contract BridgeBase {
         token = IToken(_token);
     }
 
-    function burn(address account, uint256 amount) external {
+    function burn(
+        address to,
+        uint256 amount,
+        uint256 nonce,
+        bytes calldata signature
+    ) external {
+        require(
+            processedNonces[msg.sender][nonce] == false,
+            "TRANSFER_ALREADY_PROCESSED"
+        );
+        processedNonces[msg.sender][nonce] = true;
         token.burn(msg.sender, amount);
         emit Transfer(
             msg.sender,
-            account,
+            to,
             amount,
             block.timestamp,
             nonce,
+            signature,
             Step.Burn
         );
-        nonce++;
     }
 
     function mint(
-        address account,
+        address from,
+        address to,
         uint256 amount,
-        uint256 otherChainNonce
-    ) external onlyOwner {
+        uint256 nonce,
+        bytes calldata signature
+    ) external {
+        bytes32 message = prefixed(
+            keccak256(abi.encodePacked(from, to, amount, nonce))
+        );
+        require(recoverSigner(message, signature) == from, "WRONG_SIGNATURE");
         require(
-            processedNonces[otherChainNonce] == false,
+            processedNonces[from][nonce] == false,
             "TRANSFER_ALREADY_PROCESSED"
         );
-        processedNonces[otherChainNonce] = true;
-        token.mint(account, amount);
+        processedNonces[from][nonce] = true;
+        token.mint(to, amount);
         emit Transfer(
-            msg.sender,
-            account,
+            from,
+            to,
             amount,
             block.timestamp,
-            otherChainNonce,
+            nonce,
+            signature,
             Step.Mint
         );
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "ONLY_OWNER");
-        _;
+    function prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
+            );
+    }
+
+    function recoverSigner(bytes32 message, bytes memory sig)
+        internal
+        pure
+        returns (address)
+    {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        internal
+        pure
+        returns (
+            uint8,
+            bytes32,
+            bytes32
+        )
+    {
+        require(sig.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
     }
 }
