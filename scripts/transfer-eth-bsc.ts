7a09f09c8ffa6a4f7e138374b2333fd73f92d5a1
Miquel Àngel Cabot <miquel@unizen.io>
2021-11-16 13:27:08 +0100

    feat: sign message when transfer to bridgeETH

--- scripts/transfer-eth-bsc.ts
+++ scripts/transfer-eth-bsc.ts
@@ -8,13 +8,20 @@ import * as BRIDGEETH from '../deployments/rinkeby/BridgeETH.json';
  * main function
  */
 async function main() {
+  const nonce = 1; //Need to increment this for each new transfer
   const [owner] = await ethers.getSigners();
   const amount = 1000;
+
+  const message = ethers.utils.solidityKeccak256(
+    ['address', 'address', 'uint256', 'uint256'],
+    [owner.address, owner.address, amount, nonce]
+  );
+  const signature = await owner.signMessage(message);
 
   const BridgeETH = await ethers.getContractFactory('BridgeETH');
   const bridgeETH = await BridgeETH.attach(BRIDGEETH.address);
 
-  await bridgeETH.burn(owner.address, amount);
+  await bridgeETH.burn(owner.address, amount, nonce, signature);
 }
 
 main()
